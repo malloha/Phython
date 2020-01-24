deb30ea1437789f5ea4b697b5064d20b540ddf22
Maleeha <malehahasan@gmail.com>
2020-01-24 10:38:53 -0500

    Loops and variables practice

---
+++ loops.py
@@ -0,0 +1,15 @@
+items = [1,254,4,5]
+
+
+item_len = len(items)
+
+print(item_len)
+
+# for i in items:
+#     print(i)
+
+my_range = range(len(items),2)
+print(my_range)
+
+for i in range(item_len ):
+    print(items[i])
